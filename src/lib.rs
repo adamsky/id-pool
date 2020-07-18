eb8e65bf2309dc7ba65f2261648adb64cfc3d603
adamsky <adamsky@enum.space>
2020-07-18 23:31:52 +0200

    comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
+//! Create and recycle integer ids using a ranged pool.
+
 #[derive(Debug, Clone)]
 pub struct IdPool {
     free: Vec<std::ops::Range<usize>>,
 }
 
 impl IdPool {
+    /// Creates a new `IdPool` with a default range.
     pub fn new() -> Self {
         Self {
             free: vec![1..usize::MAX],
         }
     }
 
+    /// Returns a new id.
     pub fn request_id(&mut self) -> Option<usize> {
+        // always work on the last range on the list
         let range = self.free.last_mut().unwrap();
+        // get the first number from the range
         let id = range.start;
+        // increment range starting point
         range.start = range.start + 1;
         Some(id)
     }
